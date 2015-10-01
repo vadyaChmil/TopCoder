ad46c51c3210aa8c8ac3458107c0be55c2382346
vadyaChmil <vadya.chmil@gmail.com>
2015-10-01 22:33:24 +0300

    564 SRM 424 DIV 2_250

---
+++ src/com/topcoder/room564/srm424/div2_250/Main.java
@@ -0,0 +1,10 @@
+package com.topcoder.room564.srm424.div2_250;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String spell = "AZ";
+		System.out.println(new MagicSpell().fixTheSpell(spell));
+	}
+
+}
