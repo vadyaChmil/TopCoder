af2d812380e3d309d3751f1d750e08fb5ddc383a
vadyaChmil <vadya.chmil@gmail.com>
2015-10-01 22:31:57 +0300

    379 SRM 334 DIV 1_250

---
+++ src/com/topcoder/room379/srm334/div1_250/Main.java
@@ -0,0 +1,10 @@
+package com.topcoder.room379.srm334.div1_250;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] numbers = { "ABC", "BCA" };
+		System.out.println(new EncodedSum().maximumSum(numbers));
+	}
+
+}
