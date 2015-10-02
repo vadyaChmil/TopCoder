0435f469756b87c9cf9605cfbdb287acf188d243
vadyaChmil <vadya.chmil@gmail.com>
2015-10-02 22:46:27 +0300

    376 SRM 332 DIV 2_500

---
+++ src/com/topcoder/room376/srm332/div2_500/CreatePairs.java
@@ -0,0 +1,47 @@
+package com.topcoder.room376.srm332.div2_500;
+
+import java.util.Arrays;
+
+public class CreatePairs {
+
+	public int maximalSum(int[] data) {
+		int[] negativeValues = null;
+		int[] positiveValues = data;
+		Arrays.sort(data);
+		for (int index = data.length - 1; index >= 0; index--) {
+			if (data[index] <= 0) {
+				negativeValues = new int[index + 1];
+				positiveValues = new int[data.length - index - 1];
+				System.arraycopy(data, index + 1, positiveValues, 0, positiveValues.length);
+				System.arraycopy(data, 0, negativeValues, 0, negativeValues.length);
+				break;
+			}
+		}
+		int maximalSum = 0;
+		for (int index = positiveValues.length - 1; index >= 0; index--) {
+			if (positiveValues[index] == 1) {
+				maximalSum += positiveValues[index];
+				continue;
+			}
+			if (index > 0) {
+				maximalSum += positiveValues[index] * positiveValues[index - 1];
+				index--;
+			} else {
+				maximalSum += positiveValues[index];
+			}
+		}
+		if (negativeValues == null) {
+			return maximalSum;
+		}
+		for (int index = 0; index < negativeValues.length; index++) {
+			if (index < negativeValues.length - 1) {
+				maximalSum += negativeValues[index] * negativeValues[index + 1];
+				index++;
+			} else {
+				maximalSum += negativeValues[index];
+			}
+		}
+		return maximalSum;
+	}
+
+}
---
+++ src/com/topcoder/room376/srm332/div2_500/Main.java
@@ -0,0 +1,10 @@
+package com.topcoder.room376.srm332.div2_500;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] data = { 0, 1, 2, 4, 3, 5 };
+		System.out.println(new CreatePairs().maximalSum(data));
+	}
+
+}
